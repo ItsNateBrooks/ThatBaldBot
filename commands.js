ba9f9987561938bd49f3391feff90165af75a130
ItsNateBrooks <ItsNateBrooks@gmail.com>
2022-04-27 19:23:43 -0400

    RPS Refactor
    The !rps command was widely refactored to be more readable and have more concise logic

--- commands.js
+++ commands.js
@@ -1,84 +1,55 @@
-let scoreUsers = 0
-let scoreBot = 0
 const gif = require("./commands/gif.js")
 
 function getRandomInt(max) {
     return Math.floor(Math.random() * max);
 }
 
+function assign_RPS_Score(RPS_String) {
+    return RPS_String === ("rock") ? 0 : (RPS_String === ("paper") ? 1 : (RPS_String === ("scissors") ? 2 : 100))
+}
+
 module.exports = async function (msg){
     if(msg.author.bot) return;
-    if(msg.content.toLowerCase().includes("xqc") || msg.content.toLowerCase().includes("elden ring")){
+    let tokens = msg.content.toLowerCase().split(" ")
+
+    if(tokens.includes("xqc") || tokens.includes("elden ring")){
         let url = `https://g.tenor.com/v1/search?q=no-one-cares&key=${process.env.TENORKEY}}&limit=8`
         let response = await fetch(url)
         let json = await response.json()
         let index = Math.floor(Math.random() * json.results.length)
         msg.reply(json.results[index].url)
     }
-    if(msg.content.toLowerCase().includes("oof") ){
+
+    if(tokens.includes("oof") ){
         msg.reply("https://tenor.com/view/thats-rough-buddy-avatar-the-last-airbender-zuko-gif-17596756")
     }
-    if(msg.content.toLowerCase().includes("boi") ){
+    if(tokens.includes("boi") ){
         msg.reply("https://tenor.com/view/spongebob-breath-in-inhale-gif-11964324")
     }
-    if(msg.content.toLowerCase().includes("!rps")){
+    if(tokens[0] == ("!rps")){
 
-        let rpsBot = getRandomInt(3)
-        let rpsBotString = ""
+        const rpsArr = ["Rock","Paper","Scissors"];
+        let rpsBot = getRandomInt(rpsArr.length)
+        let rpsBotString = rpsArr[rpsBot];
         let rpsUser = 100
 
-        switch (rpsBot){
-            case 0:
-                rpsBotString = "Rock"
-            break
-            case 1:
-                rpsBotString = "Paper"
-            break
-            case 2:
-                rpsBotString = "Scissors"
-            break
-        }
-  
-        if(msg.content.toLowerCase().includes("rock"))
-            rpsUser = 0
-        if(msg.content.toLowerCase().includes("paper"))
-            rpsUser = 1
-        if(msg.content.toLowerCase().includes("scissors"))
-            rpsUser = 2
-
+        rpsUser = assign_RPS_Score(tokens[1]) 
+        console.log("UserString: " + tokens[1] + " UserScore :"+rpsUser+ " BotString :"+rpsBotString+ " BotScore :"+rpsBot)      
+        
         switch (rpsUser){
-            case 100:
-                msg.reply('Bruh, do you not know how this works? Such a BOZO, anyways, use !rps <Rock, Paper, or Scissors> **COUGH** *literal noob* **COUGH**')
-            break
             case rpsBot:
-                msg.reply(rpsBotString)
-                msg.reply("We for real tied I guess, lets go again...") // \n       Score:\nBot: " + scoreBot +" vs Users: " + scoreUsers
+                msg.reply(rpsBotString+"\nWe for real tied I guess, lets go again...")
             break
             case rpsBot+1:
-                msg.reply(rpsBotString)
-                scoreUsers++
-                msg.reply("BRO, HOW DID YOU BEAT ME CHEATER!!!1!!!1!!!")//\n       Score:\nBot: " + scoreBot +" vs Users: " + scoreUsers
-                
-            break
             case rpsBot-2:
-                msg.reply(rpsBotString)
-                scoreUsers++
-                msg.reply("BRO, HOW DID YOU BEAT ME CHEATER!!!1!!!1!!!")//\n       Score:\nBot: " + scoreBot +" vs Users: " + scoreUsers
+                msg.reply(rpsBotString+"\nBRO, HOW DID YOU BEAT ME CHEATER!!!1!!!1!!!")
             break
             case rpsBot-1:
-                msg.reply(rpsBotString)
-                scoreBot++
-                msg.reply("Cuh, I new Id win")//\n       Score:\nBot: " + scoreBot +" vs Users: " + scoreUsers
-                break
-            break
             case rpsBot+2:
-                msg.reply(rpsBotString)
-                scoreBot++
-                msg.reply("Cuh, I new Id win")//\n       Score:\nBot: " + scoreBot +" vs Users: " + scoreUsers
-                break
+                msg.reply(rpsBotString+"\nCuh, I new Id win")
             break
             default:
-                msg.reply('Bruh, do you not know how this works? Such a BOZO, anyways, use !rps <Rock, Paper, or Scissors> **COUGH** *literal noob* **COUGH**')
+                msg.reply("Bruh, do you not know how this works? Such a BOZO, anyways, use !rps <Rock, Paper, or Scissors> **COUGH** *literal noob* **COUGH**")
             break
         
         }
