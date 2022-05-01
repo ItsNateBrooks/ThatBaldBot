c5a07f43d6f0e9f98f13fc3746cecfe25cc42f41
ItsNateBrooks <ItsNateBrooks@gmail.com>
2022-04-30 21:21:22 -0400

    !ttt REFACTOR
    Many aspects of the !ttt command have been refactored and improved, several functions have been added to remove the various unnecessary comparison statements. Also several lookup tables have been implemented to completely eliminate the main switch that determines what number the user just entered. No bot or two player functions yet, still solely single player.

--- commands/ttt.js
+++ commands/ttt.js
@@ -1,16 +1,9 @@
 let boardSet = false
 let marksSet = false
-let playingGame = false
 let gameWon = false
-let oneMarked = false
-let twoMarked = false
-let threeMarked = false
-let fourMarked = false
-let fiveMarked = false
-let sixMarked = false
-let sevenMarked = false
-let eightMarked = false
-let nineMarked = false
+let playingGame = false
+let locs = []
+let boardSpaceMarked = [false, false, false, false, false, false, false, false, false]
 let userMark = " "
 let botMark = " "
 let displayBoard = [
@@ -28,51 +21,17 @@ function getRandomInt(max) {
     return Math.floor(Math.random() * max);
 }
 
-function checkDiags(board){
-    if(board[1][1] == board[0][0] && board[1][1] == board[2][2]){
-        gameWon = true
-    }
-    if(board[1][1] == board[2][0] && board[1][1] == board[0][2]){
-        gameWon = true
-    }
-}
-function checkColumns(board){
-    if(board[0][0] == board[0][1] && board[0][0] == board[0][2] ){
-        gameWon = true
-    }
-    if(board[1][0] == board[1][1] && board[1][0] == board[1][2] ){
-        gameWon = true
-    }
-    if(board[2][0] == board[2][1] && board[2][0] == board[2][2] ){
-        gameWon = true
-    }
-}
-function checkRows(board){
-    if(board[0][0] == board[1][0] && board[0][0] == board[2][0] ){
-        gameWon = true
-    }
-    if(board[0][1] == board[1][1] && board[0][1] == board[2][1] ){
-        gameWon = true
-    }
-    if(board[0][2] == board[1][2] && board[0][2] == board[2][2] ){
-        gameWon = true
-    }
-}
-
-function botTurn(board){
-    
-}
-
 function reset(){
     boardSet = false
     marksSet = false
     playingGame = false
     gameWon = false
+    boardSpaceMarked = [false, false, false, false, false, false, false, false, false]
     userMark = " "
     botMark = " "
     displayBoard = [
         "#","-","-","-","#\n",
-        "|","1 ","2","3","|\n",
+        "|"," 1","2","3","|\n",
         "|","4","5","6","|\n",
         "|","7","8","9","|\n",
         "#","-","-","-","#\n",]
@@ -80,6 +39,33 @@ function reset(){
     trueBoard = [["0","1","2"],
                  ["3","4","5"],
                  ["6","7","8"]]
+}
+
+function compare3Vals(arg1, arg2, arg3) {
+    return arg1 === arg2 ? (arg1 === arg3 ? true : false) : false
+}
+
+function checkDiags(board){
+    let diag1 = compare3Vals(board[1][1], board[0][0], board[2][2])
+    let diag2 = compare3Vals(board[1][1], board[2][0], board[0][2])
+
+    gameWon = diag1 == true ? true : (diag2 == true ? true : gameWon)
+}
+
+function checkColumns(board){
+    let row1 = compare3Vals(board[0][1], board[0][0], board[0][2])
+    let row2 = compare3Vals(board[1][1], board[1][0], board[1][2])
+    let row3 = compare3Vals(board[2][1], board[2][0], board[2][2])
+
+    gameWon = row1 == true ? true : (row2 == true ? true : (row3 == true ? true : gameWon))
+}
+
+function checkRows(board){
+    let col1 = compare3Vals(board[1][0], board[0][0], board[2][0])
+    let col2 = compare3Vals(board[1][1], board[0][1], board[2][1])
+    let col3 = compare3Vals(board[1][2], board[0][2], board[2][2])
+
+    gameWon = col1 == true ? true : (col2 == true ? true : (col3 == true ? true : gameWon))
 }
 
 function checkWin(board, msg) {
@@ -92,6 +78,67 @@ function checkWin(board, msg) {
         reset()
     }
 }
+
+function markHandler (msg, boardMarkedIndex, disBoardLoc, trueBoardLoc1, trueBoardLoc2) {
+    if(boardSpaceMarked[boardMarkedIndex]){
+        msg.reply("Bruv, you can't just place where someone's already placed before, go again... ")
+    }
+    else{
+        displayBoard[disBoardLoc] = userMark
+        trueBoard[trueBoardLoc1][trueBoardLoc2] = userMark
+        msg.reply("Board:\n" + displayBoard.join(" ") + "\n ")
+        boardSpaceMarked[boardMarkedIndex] = true
+    }
+}
+
+function getCoordNums (loc) {
+    return {
+      1: '0 0',
+      2: '0 1',
+      3: '0 2',
+      4: '1 0',
+      5: '1 1',
+      6: '1 2',
+      7: '2 0',
+      8: '2 1',
+      9: '2 2'
+    }[loc]
+}
+  function getLocationNum (loc) {
+    return {
+      1: '6',
+      2: '7',
+      3: '8',
+      4: '11',
+      5: '12',
+      6: '13',
+      7: '16',
+      8: '17',
+      9: '18'
+    }[loc]
+}
+
+function reset(){
+    boardSet = false
+    marksSet = false
+    playingGame = false
+    gameWon = false
+    userMark = " "
+    botMark = " "
+    displayBoard = [
+        "#","-","-","-","#\n",
+        "|"," 1","2","3","|\n",
+        "|","4","5","6","|\n",
+        "|","7","8","9","|\n",
+        "#","-","-","-","#\n",]
+
+    trueBoard = 
+    [["0","1","2"],
+     ["3","4","5"],
+     ["6","7","8"]]
+}
+
+
 
 module.exports = function (msg, args){
     
@@ -114,84 +161,14 @@ module.exports = function (msg, args){
         }else {
             msg.reply("Bruh, I just said reply with !ttt X or !ttt 0")
         }
-    }else if(playingGame){
-        switch (args[0]){
-            case "1":
-                if(oneMarked == true)
-                    break
-                displayBoard[6] = userMark
-                trueBoard[0][0] = userMark
-                msg.reply("Board:\n" + displayBoard.join(" ") + "\n ")
-                oneMarked = true
-            break
-            case "2":
-                if(twoMarked == true)
-                    break
-                displayBoard[7] = userMark
-                trueBoard[0][1] = userMark
-                msg.reply("Board:\n" + displayBoard.join(" ") + "\n ")
-                twoMarked = true
-            break
-            case "3":
-                if(threeMarked == true)
-                    break
-                displayBoard[8] = userMark
-                trueBoard[0][2] = userMark
-                msg.reply("Board:\n" + displayBoard.join(" ") + "\n ")
-                threeMarked = true
-            break
-            case "4":
-                if(fourMarked == true)
-                    break
-                displayBoard[11] = userMark
-                trueBoard[1][0] = userMark
-                msg.reply("Board:\n" + displayBoard.join(" ") + "\n ")
-                fourMarked = true
-            break
-            case "5":
-                if(fiveMarked == true)
-                    break
-                displayBoard[12] = userMark
-                trueBoard[1][1] = userMark
-                msg.reply("Board:\n" + displayBoard.join(" ") + "\n ")
-                fiveMarked = true
-            break
-            case "6":
-                if(sixMarked == true)
-                    break
-                displayBoard[13] = userMark
-                trueBoard[1][2] = userMark
-                msg.reply("Board:\n" + displayBoard.join(" ") + "\n ")
-                sixMarked = true
-            break
-            case "7":
-                if(sevenMarked == true)
-                    break
-                displayBoard[16] = userMark
-                trueBoard[2][0] = userMark
-                msg.reply("Board:\n" + displayBoard.join(" ") + "\n ")
-                sevenMarked = true
-            break
-            case "8":
-                if(eightMarked == true)
-                    break
-                displayBoard[17] = userMark
-                trueBoard[2][1] = userMark
-                msg.reply("Board:\n" + displayBoard.join(" ") + "\n ")
-                eightMarked = true
-            break
-            case "9":
-                if(nineMarked == true)
-                    break
-                displayBoard[18] = userMark
-                trueBoard[2][2] = userMark
-                msg.reply("Board:\n" + displayBoard.join(" ") + "\n ")
-                nineMarked = true
-            break
-            default:
-                msg.reply("Bruh, I just said reply with !ttt <1-9> with 1 being the top left and 9 being the bottom right")
-            break
-        }
+    }else if(playingGame && args[0] != undefined){
+
+        let cords = getCoordNums(args[0]).split(" ")
+        markHandler(msg, parseInt(args[0])-1, parseInt(getLocationNum(args[0])), parseInt(cords[0]), parseInt(cords[1]))
         checkWin(trueBoard, msg)
+        
+    }
+    else{
+        msg.reply("Bruh, I just said reply with !ttt <1-9> with 1 being the top left and 9 being the bottom right")
     }
 }
